73cd87a9ad7756a807cf2a0b650c71c00e128bc4
Nabarun Chatterjee <nabarun.chatterjee@gmail.com>
2018-06-08 20:49:18 +0530

    Initial files

---
+++ tmux/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/tmux-powerlinerc ~/.tmux-powerlinerc
+ln -s $(pwd) ~/.tmux
---
+++ vim/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -x
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd) ~/.vim
